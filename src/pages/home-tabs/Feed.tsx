a8b71608c2adcd2677db0d5f9904030a298a76d9
Diether012524 <labisdiether345@gmail.com>
2026-04-22 15:58:25 +0800

    added objects

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,6 +1,19 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButtons, IonContent, IonHeader, IonLabel, IonList, IonListHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 
 const Feed: React.FC = () => {
+
+
+     const labels = [ 
+    {name: 'Pokémon Yellow'},
+     {name: 'Mega Man X'},
+      {name: 'The Legend of Zelda'},
+       {name: 'Pac-Man'},
+       {name: 'Super Mario World'}
+       
+  ]
+
+
+
    return(
     <IonPage>
         <IonHeader>
@@ -14,11 +27,20 @@ const Feed: React.FC = () => {
         </IonHeader>
         <IonContent className="ion-padding" >
             <h1>Feed</h1>
-        </IonContent>
-    </IonPage>
 
-   );
 
-}
+            {labels.map((item, index) =>(
+                <IonList>
+                  <IonListHeader>
+                    <IonLabel>{item.name}</IonLabel>
+                  </IonListHeader>
+             </IonList>
 
+           ))}
+
+              </IonContent>
+              </IonPage>
+      );
+
+};
 export default Feed;
